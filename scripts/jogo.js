6b6dea824f0d9d62cd2a363de2a707e803d86763
Charles Gabriel <charlesgabriel@hotmail.com.br>
2022-05-29 15:25:42 -0300

    implementado captura de teclas e preenchimento dos espaços com letras

--- scripts/jogo.js
+++ scripts/jogo.js
@@ -1,23 +1,53 @@
 const ul = $class('jogo-erros');
-let palavra = '';
+let palavra = [];
+let letras = [];
+let teclas = [];
 
 document.body.addEventListener('keypress', function(event) {
-    console.log(event)
+    const tecla = event.key;
+    teclas.push(tecla);
+    limparCampos();
+    compararLetras(tecla);
 });
+
+function limparCampos() {
+    ul[0].textContent = "";
+}
 
 function novoJogo() {
     const erros = [];
     
     palavra = palavras[Math.floor(Math.random() * palavras.length)];
 
-    montarLi(palavra[0]);
+    console.log(palavra);
+    montarPalavra(palavra[0]);
+
+    montarLi(letras);
 }
 
-function montarLi(palavra) {
-    const letras = palavra.split('');
+function compararLetras(tecla) {
+    if(palavra[0].includes(tecla)) {
+        montarLi(letras);
+    }
+}
+
+function montarPalavra(palavra) {
+    letras = palavra.split('');
+}
+
+function montarLi(letras) {
     letras.forEach(function(letra) {
         var li = create('li');
-        li.textContent = letra;
+        li.classList.add("jogo-letras");
+        if(teclas.length > 0) {
+            teclas.forEach(function(tecla) {
+                if(tecla == letra) {
+                    li.textContent = letra;
+                }
+            });
+        } else {
+            li.textContent = "";
+        }
         ul[0].appendChild(li);
     });
 }
